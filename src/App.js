67be0b48b9d6e7d8ca802db6762d40904ad4b1cb
Your Name <sj.pune1995@gmail.com>
2020-09-05 11:27:58 +0530

    Successful Auth call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Resolver from './application/Resolver';
+import ErrorBoundary from './application/ErrorBoundary';
 
 function App() {
   return (
     <div className="App">
-      <Resolver></Resolver>
+      <ErrorBoundary>
+      <Resolver/>
+      </ErrorBoundary>
     </div>
   );
 }
--- src/application/Resolver.js
+++ src/application/Resolver.js
@@ -12,27 +12,29 @@ class Resolver extends Component{
     componentDidMount() {
         axios.get(`https://www.universal-tutorial.com/api/getaccesstoken`
         ,{
-          headers: {auth_token:'A_HnuYBEtnJ2gaL3anpx7iRtLla9BLE3Eje78nrEeiHGrG7RhzDTRVa6dihaTcHkIH0'}})
+          headers: {'api-token':'bUc49N0IVVry28oIAMr6PI678gAebpJDgNCifnQfEIAFr4o8TlNtKsQ6zBJahOUKJcw',
+          'user-email': 'sj.pune95@gmail.com'}})
           .then(res => {
             console.log(res.data);
             const auth = res.data.auth_token;          
             this.setState({ auth });
-            console.log(this.state.auth)
-            //this.setState({ persons });
+            console.log(`Bearer ${this.state.auth}`)
+            //this.setState({ persons  });
+            return axios.get(`https://www.universal-tutorial.com/api/countries/`,{
+              headers: {
+                "Authorization": `Bearer ${this.state.auth}`
+                //"Accept": "application/json"
+                //'authorization': ` ${this.state.auth}`
+              }
+            
           })
-          axios.get(`https://www.universal-tutorial.com/api/countries/`,{
-            headers: {
-              "Authorization":`Bearer ${this.state.auth}`,
-  "Accept": "application/json"
-              //'authorization': ` ${this.state.auth}`
-            }
+            .then(res => {
+              console.log(`Bearer ${this.state.auth}`);
+              const countries = res.data;
+              this.setState({ countries });
+            })
+          })
           
-        })
-          .then(res => {
-            console.log(`Bearer ${this.state.auth}`);
-            const countries = res.data;
-            this.setState({ countries });
-          })
           
       }
     render () {
